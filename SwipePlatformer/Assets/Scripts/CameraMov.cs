aa2d48c00f36d80df43198555f6db7c0fb4a7000
korteh <43537663+korteh@users.noreply.github.com>
2018-09-24 16:48:14 +0300

    hop[e this will work

---
+++ SwipePlatformer/Assets/Scripts/CameraMov.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraMov : MonoBehaviour {
+
+    public Transform target;
+
+    public float SmoothSpeed = 0.5f;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 desPos = target.position + offset;
+        Vector3 SmPos = Vector3.Lerp(transform.position, desPos, SmoothSpeed * Time.deltaTime);
+        transform.position =SmPos;
+        transform.LookAt(target);
+    }
+}
